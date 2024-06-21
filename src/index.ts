a4405b01c27b04a183b232b2adb1f34d2a4aad18
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-06-22 00:49:53 +0530

    request counter using prom-client middleware added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express, {Request, Response} from "express";
 import { middleware } from "./middlewares/middleware";
+import client from "prom-client";
+import { requestCountMiddleWare } from "./metrics";
 
 const app = express();
 
 app.use(express.json());
-app.use(middleware);
+//app.use(middleware);
+app.use(requestCountMiddleWare);
 
 app.get("/user", (req: Request,res: Response)=>{
-    res.status(200).send({
+    res.status(200).json({
         name:"swata swayam dash",
         age: 21
     })
@@ -15,10 +18,16 @@ app.get("/user", (req: Request,res: Response)=>{
 
 app.post("/user", (req:Request, res:Response) => {
     const user = req.body.user;
-    res.status(200).send({
+    res.status(200).json({
         ...user,
         id:1
     })
+})
+
+app.get("/metrics", async(req,res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
 })
 
 app.listen(3000, ()=>{
